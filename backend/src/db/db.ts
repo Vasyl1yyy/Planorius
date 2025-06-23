bfa3e8b6fa9253aa8a4b3cd92377639cb2106f5b
Vasyl1yyy <tanksgold967@gmail.com>
2025-06-23 18:05:07 +0300

    add tasks schema: define tasks table structure for database integration

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -2,12 +2,14 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import dotenv from 'dotenv';
 import { users } from './schema/users';
+import { tasks } from './schema/tasks';
 
 dotenv.config();
 
 const db = drizzle(process.env.DATABASE_URL, {
   schema: {
     users,
+    tasks,
   },
 });
 
---
+++ backend/src/db/schema/tasks.ts
@@ -0,0 +1,15 @@
+import { pgTable, serial, text, integer, timestamp } from 'drizzle-orm/pg-core';
+
+export const tasks = pgTable('tasks', {
+  id: serial('id').primaryKey(),
+  userId: integer('user_id').notNull(),
+  done: integer('done').notNull().default(0),
+  title: text('title').notNull(),
+  difficulty: integer('difficulty').notNull().default(0),
+  proirity: integer('priority').notNull().default(0),
+  tag: text('tag').notNull().default(''),
+  date: timestamp('date').notNull().defaultNow(),
+  time: integer('time').notNull().default(0),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
